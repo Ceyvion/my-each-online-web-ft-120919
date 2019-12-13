8e85f7d80557429c24e4ef62230889da4e94a6fd
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-13 23:02:17 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(array)
+  #use while loop to iterate 
+  #then yield to each element in the block 
+  #then returning that value 
   # code here
+  counter += 0 
+  if block_given?
+    while counter < array.length 
+  yield |words| 
+    end 
+
+
+
+
+end 
+
 end
