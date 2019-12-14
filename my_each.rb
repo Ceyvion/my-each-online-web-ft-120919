2ba00fc81bd326d0f07dcc5a2881928e5470eccb
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-14 00:17:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,15 +3,16 @@ def my_each(array)
   #then yield to each element in the block 
   #then returning that value 
   # code here
-  counter += 0 
+  counter = 0 
   if block_given?
     while counter < array.length 
   yield array 
-    end 
+  counter +=1 
+end 
+array
 
-
-my_each 
-
+my_each do |numbers| 
+  puts numbers
 end 
 
 end
